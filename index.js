133afd3c105ea755f5cbd377e3f8e66ac5a17c5e
Amila Welihinda <amilajack@gmail.com>
2018-08-13 13:09:00 -0700

    Misc

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const createTestCafe = require('testcafe');
+
 if (!process.env.CI) {
     require('dotenv').config()
 }
+
+createTestCafe('localhost', 1337, 1338)
+    .then(testCafe => {
+        if (!process.env.CI) {
+            require('dotenv').config()
+        }
+        
+        testCafe
+            .createRunner()
+            .src('./index.e2e.js')
+            .browsers('saucelabs:Chrome@beta:Windows 10')
+            .run();
+    });
